9fae4f1f1d5cd24b830a835edf14c890a2710c26
nguyen phat <nguyenvanphat20999@gmail.com>
2021-05-14 00:46:52 +0700

    ng.va.ph

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const route = require("./routes");
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 // const port = 3001;
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 app.engine("handlebars", exphdbs());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "/resources/views"));
