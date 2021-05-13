5e7be1fa603e634b1ff7c4ba915043647a401de6
nguyen phat <nguyenvanphat20999@gmail.com>
2021-05-13 23:47:55 +0700

    qqqq

--- src/index.js
+++ src/index.js
@@ -14,4 +14,10 @@ app.set("views", path.join(__dirname, "/resources/views"));
 
 route(app);
 
-app.listen(process.env.PORT, function () {});
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
